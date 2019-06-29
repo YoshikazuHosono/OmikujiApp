cba83e4f3ac61e6b458a0cd2da41770a0d61f05d
YoshikazuHosono <yyhhwork@gmail.com>
2019-06-30 00:10:13 +0900

    メイン画面を作成

--- app/src/main/java/com/example/omikujiapp/OmikujiAct.kt
+++ app/src/main/java/com/example/omikujiapp/OmikujiAct.kt
@@ -2,11 +2,14 @@ package com.example.omikujiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.omikuji_act.*
 
-class MainActivity : AppCompatActivity() {
+class OmikujiAct : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.omikuji_act)
+
+        hello_view.text = "τの会のホソノ@おみくじアプリ"
     }
 }
